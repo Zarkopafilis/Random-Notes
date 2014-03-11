5d432698f042142901c4768d8fc61b1e7d55e46b
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-03-11 22:27:19 +0200

    Update Reflection.java

--- Reflection.java
+++ Reflection.java
@@ -2,3 +2,4 @@
 Method method = System.out.getClass().getMethod("println", String.class);//how to use a method with reflection. Get the class (With proper hierarchy) and then
 method.invoke(System.out, "invoke parameters");	//invoke! Params = Method's class , parameters of the method!
 /***Class***/
+TO do

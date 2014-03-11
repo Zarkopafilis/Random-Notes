d39c6a35ca9e8acb19e3cb67184eec526a9e2fe9
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-03-11 22:26:25 +0200

    Create Reflection.java
    How to use reflection in Java

---
+++ Reflection.java
@@ -0,0 +1,4 @@
+/***Method***/
+Method method = System.out.getClass().getMethod("println", String.class);//how to get a method with reflection. Get the class (With proper hierarchy) and then
+method.invoke(System.out, "invoke parameters");	//invoke! Params = Method's class , parameters of the method!
+/***Class***/

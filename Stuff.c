f2c7490c502600021858878b7b1f8859cdc957dd
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-03-12 15:39:35 +0200

    Update Stuff.c

--- Stuff.c
+++ Stuff.c
@@ -2,7 +2,9 @@ I prefer eclipse kepler in Ubuntu/Linux (and I am not doing any C/C++ developmen
 I have not tried on a mac). You need to download the CDT after downloading eclipse (it's a plugin , tons of tutorials 
 out there!).GCC Compiler FTW!
 
-
+*type to refer to the value stored inside the memory address of the type that is pointed at.
+type(pointer) to refer to the memory address of the pointed type.
+&type to refer to the type's memory address.
 
 scanf("%type", *type);
 printf("%type..." , type...);

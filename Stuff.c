03fd1c758bd29498ea05b5e4630bfdfc66b9dd4d
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-03-12 15:35:58 +0200

    Create Stuff.c

---
+++ Stuff.c
@@ -0,0 +1,8 @@
+I prefer eclipse kepler in Ubuntu/Linux (and I am not doing any C/C++ development on windows any more ,
+I have not tried on a mac). You need to download the CDT after downloading eclipse (it's a plugin , tons of tutorials 
+out there!).GCC Compiler FTW!
+
+
+
+scanf("%type", *type);
+printf("%type..." , type...);

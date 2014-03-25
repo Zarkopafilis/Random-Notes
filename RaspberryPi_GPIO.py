62dfb2855702b8f2d72462eb7277e6443ae16120
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-03-25 21:29:26 +0200

    Create RaspberryPi_GPIO.py

---
+++ RaspberryPi_GPIO.py
@@ -0,0 +1,19 @@
+#I sometimes forget some stuff
+
+#import
+import RPi.GPIO as GPIO
+
+#numbering that should be used (important), with GPIO.BOARD you refer to the pins with the way that they are counted 
+#physically (not by other weird numbers...)
+GPIO.setmode(GPIO.BOARD)
+
+#set up the way each pin works
+GPIO.setup(PIN_NUMBER , GPIO.OUT/GPIO.IN )
+
+#output 3.3 Volts
+GPIO.output(PIN_NUMBER , True/False )
+
+#before you terminate the program , use this line of code to ensure a 'clean' exit - it basically resets everything
+#(stuff you have already setted up , unbinds pins reserved from the program , allowing them to be used again without 
+#warnings). Just do it!
+GPIO.cleanup()

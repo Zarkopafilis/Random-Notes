8eb743b6ed7dd9211266b51e27de23c5dabe28dd
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-10-03 13:22:41 +0300

    Update Scala.scala

--- Scala.scala
+++ Scala.scala
@@ -1,1 +1,4 @@
 Traits are amazing....partial implementation FTW
+
+Theres an issue with all these high level stuff and lambdas and stuff :
+Compiled bytecode gets bigger -> stack traces get HUGE and not easy to understand.

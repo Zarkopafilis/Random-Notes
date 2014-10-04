150d25c64d768c73f6d3ec760b6f8fd409224737
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-10-04 22:55:38 +0300

    Update Scala.scala

--- Scala.scala
+++ Scala.scala
@@ -2,3 +2,5 @@ Traits are amazing....partial implementation FTW
 
 Theres an issue with all these high level stuff and lambdas and stuff :
 Compiled bytecode gets bigger -> stack traces get HUGE and not easy to understand.
+
+def func(call_by_name: => Something , call_by_value: Something)

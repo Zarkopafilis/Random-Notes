7a505f052232bf0222e82867930e4f9f40882745
Theodoros Ntakouris <zarkopafilis@gmail.com>
2014-10-30 23:03:03 +0200

    Update FinallyBlocks.java

--- FinallyBlocks.java
+++ FinallyBlocks.java
@@ -4,3 +4,7 @@ Its supposed to be the last but eh...least safety net.
 *Will stack up and be queued as other stuff take place (Garbage collection etc...)
 Better add post-exception code on catch block.
 Finally blocks may be sooooo delayed till they get executed!
+
+Notes for exceptions:
+If there is no per-exception-type different handling , instead of multi catch blocks I prefer to just catch 'Exception' 
+superclass instead.

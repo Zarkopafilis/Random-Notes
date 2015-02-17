94b29aa27e27abc707e67dae8fe6ce8046276e15
Theodoros Ntakouris <zarkopafilis@gmail.com>
2015-02-17 23:06:07 +0200

    Update fileExtractor.py

--- fileExtractor.py
+++ fileExtractor.py
@@ -6,6 +6,12 @@ def unique(seq):
     seen = set()
     return [seen.add(x) or x for x in seq if x not in seen]
 
+def safeDictGet(dictionary , key):
+    if(key in dictionary):
+        return dictionary[key]
+    else:
+        return 0
+
 print "---File Extractor---"
 print ""
 
@@ -15,6 +21,7 @@ root = unicode(raw_input("Directory to extract files from: "))
 print "Analyzing filetypes..."
 
 extensions = []
+nums = {}
 
 i=0
 
@@ -22,6 +29,7 @@ for path, subdirs, files in os.walk(root):
     for name in files:
         ext = unicode(name).split(".")[-1]
         extensions.append(ext)
+        nums[ext] = safeDictGet(nums , ext) + 1
         i+=1
 
 extensions = unique(extensions)
@@ -29,12 +37,12 @@ extensions = unique(extensions)
 print "File types available: "
 
 for ext in extensions:
-    print ext
+    print ext + " - " + str(nums[ext]) + " files"
 
 print "Total files: " + str(i)
 
 target = unicode(raw_input("Directory to save extracted files to: "))
-pattern = unicode(raw_input("File extensions (Split by -): "))
+pattern = unicode(raw_input("File extensions (Split by '-'): "))
 
 pattern = pattern.split("-")
 
